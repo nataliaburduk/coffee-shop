44ad7697d14f9b88d285a1894696a1966e7fdf3f
Natalia Burduk <nataliaburduk@gmail.com>
2023-01-21 23:01:50 +0100

    Remove useless files

--- src/components/main-screen/main-screen.js
+++ src/components/main-screen/main-screen.js
@@ -1,23 +1,23 @@
 import { Component } from "react";
-import './main-screen.css';
-import BtMainScreen from '../bootstrapMainScreen';
+import Container from 'react-bootstrap/Container';
+import beans from '../../img/beans-title.svg';
+import './main-screen.css'
 
 class MainScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ''
-    }
-  }
-  
   render() {
-    return (   
-      <div>
-        <BtMainScreen/>
-      </div>
+    return (
+      <Container fluid className='main-screen'>
+        
+        <div className='main-title'>
+          <h1>Everything You Love About Coffee</h1>
+          <img src={beans} alt='coffee-title'/>
+          <h2 className='main-subtitle'>We makes every day full of energy and taste</h2>
+          <h2 className='main-subtitle'>Want to try our beans?</h2>
+          <button className='main-btn'>More</button>
+        </div>
+      </Container>
     ) 
   }
 }
-
 
 export default MainScreen;
