3479d3fbf0660f473b3c4ff416d7ce0a649eea4b
Natalia Burduk <nataliaburduk@gmail.com>
2023-01-24 16:31:52 +0100

    Rename scroll to top function

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,35 +1,28 @@
 import {Link} from "react-router-dom";
-import { useEffect } from 'react';
 
 import './nav-bar.css'
 
 const Navbar = ({navBarType,coffeeLogo}) => {
-  useEffect(() => {
+  const scrollToTop = () => {
     window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-  }, []);
+  };
   
   return (
     <nav className={navBarType}>
         <img src={coffeeLogo} alt='coffee-logo'className='coffee-logo'/>
         <ul className="nav-menu">
           <li className="nav-item">
-            <Link to="/" className="nav-link" onClick={() => {
-              window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-            }}>
+            <Link to="/" className="nav-link" onClick={scrollToTop}>
               Coffee house
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/our-coffee" className="nav-link" onClick={() => {
-              window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-            }}>
+            <Link to="/our-coffee" className="nav-link" onClick={scrollToTop}>
               Our coffee
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/for-your-pleasure" className="nav-link" onClick={() => {
-              window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-            }}>
+            <Link to="/for-your-pleasure" className="nav-link" onClick={scrollToTop}>
               For your pleasure
             </Link>
           </li>
